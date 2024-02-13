a250c7b71924de1b66032291f3d07121b6b8efa6
Justice Mitchell <justice.s.mitchell@gmail.com>
2024-02-13 01:35:50 -0600

    adds the initial parameters and constructors/methods for the StockManagerSingleton class

--- Homework_3/src/hw3/mediaManager/StockManagerSingleton.java
+++ Homework_3/src/hw3/mediaManager/StockManagerSingleton.java
@@ -1,5 +1,30 @@
 package hw3.mediaManager;
 
+import java.util.ArrayList;
+import hw3.media.*;
+
 public class StockManagerSingleton {
+	
+	// Data Section
+    private static final StockManagerSingleton INSTANCE = new StockManagerSingleton(); // Only one instance of StockManagerSingleton
+    private ArrayList<MediaProduct> inventory;
+    private String inventoryFilePath = "../inventory.csv";
+
+    //Private constructor (because its a singleton)
+    private StockManagerSingleton() {
+        inventory = new ArrayList<>();
+    }
+
+    // Public method to get the instance
+    public static StockManagerSingleton getInstance() {
+        return INSTANCE;
+    }
+    
+    // Assignment object methods
+    
+    public boolean initializeStock() {
+    	
+    	return false;
+    }
 
 }
